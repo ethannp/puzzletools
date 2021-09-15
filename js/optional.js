3eb0705b41a7515e78c4c1f08ce684de8518d10b
Ethan <ethananhphan@gmail.com>
2021-09-15 18:33:01 -0400

    all letters optional

---
+++ js/optional.js
@@ -0,0 +1,23 @@
+document.getElementById("query").addEventListener("input", e => {
+    refresh();
+});
+document.getElementById("input-small").addEventListener("input", e => {
+    refresh();
+});
+document.getElementById("single").addEventListener("input", e => {
+    refresh();
+});
+function refresh() {
+    let value = document.getElementById("query").value.toLowerCase().replace(/[^a-z]+/g, "");
+    let result = "";
+    for (let i = 0; i < value.length; i++) {
+        result += value[i] + "?"
+    }
+    if(document.getElementById("single").checked){
+        result = `"${result}"`
+    }
+    if (document.getElementById("input-small").value != "") {
+        result += `%26_%7B${document.getElementById("input-small").value}%7D`;
+    }
+    document.getElementById("redirect").href = `https://nutrimatic.org/?q=${result}&go=Go`
+}
